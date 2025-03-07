d2e0750a421857d804bff9110420009c3bd6f6d6
ZETA-A <56914556+ZETA-A@users.noreply.github.com>
2025-03-07 06:07:10 +0000

    💉 UI: hook과 DropdownMenu 주입

--- components/layout/navigation/Navigation.tsx
+++ components/layout/navigation/Navigation.tsx
@@ -6,10 +6,11 @@ import NavIcon from './NavIcon';
 import NavLinks from './NavLinks';
 import LanguageMenu from '@/components/ui/DropdownMenu/LanguageMenu';
 import ThemeMenu from '@/components/ui/DropdownMenu/ThemeMenu';
+import usePopupMenuStore, { PopupMenuState } from '@/hooks/popupMenuStore';
 
 export default function Navigation() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
+    const { popupMenuState, setPopupMenuState } = usePopupMenuStore();
+    const toggleNavigation = () => setPopupMenuState(PopupMenuState.Navigation);
     return (
         <nav className="realative w-full select-none flex justify-between h-16 items-center">
             {/* 블로그 이름 */}
@@ -21,7 +22,9 @@ export default function Navigation() {
                 </div>
             </Link>
             {/* 네비게이션 버튼 */}
-            <NavLinks isMenuOpen={isMenuOpen} />
+            <NavLinks
+                isMenuOpen={popupMenuState === PopupMenuState.Navigation}
+            />
 
             {/* 언어 및 테마 설정 */}
             <div className="flex space-x-3">
@@ -29,9 +32,13 @@ export default function Navigation() {
                 <ThemeMenu />
                 <button
                     className="md:hidden hover:bg-neutral-100 dark:hover:bg-neutral-700 box-content p-2 rounded-lg"
-                    onClick={() => setIsMenuOpen(!isMenuOpen)}
+                    onClick={() => toggleNavigation()}
                 >
-                    <NavIcon isMenuOpen={isMenuOpen} />
+                    <NavIcon
+                        isMenuOpen={
+                            popupMenuState === PopupMenuState.Navigation
+                        }
+                    />
                 </button>
             </div>
         </nav>
