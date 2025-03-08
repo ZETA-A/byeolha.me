ec92c2fb62aaf381e87f074d82fb5fbd299403f5
ZETA-A <56914556+ZETA-A@users.noreply.github.com>
2025-03-08 06:35:21 +0000

    🎨 CSS: global css 1차 적용 완료

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,11 +19,11 @@ export default function RootLayout({
         <html
             lang="en"
             suppressHydrationWarning={true}
-            className={`${maruburi.variable}`}
+            className={`${maruburi.variable} bg-page`}
         >
-            <body className="flex flex-col font-maruburi dark:bg-neutral-900 text-neutral-900 dark:text-neutral-100">
+            <body className="flex flex-col font-maruburi">
                 <ThemeProvider attribute="class">
-                    <div className="w-full max-w-6xl mx-auto px-8 flex flex-col min-h-screen">
+                    <div className="w-full max-w-6xl mx-auto px-4 flex flex-col min-h-screen">
                         <Navigation />
                         <main className="flex-1 max-w-2xl w-full mx-auto my-8">
                             {children}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,33 @@ export default {
     ],
     theme: {
         extend: {
+            colors: {
+                transparent: 'transparent',
+                gray: {
+                    50: 'var(--gray-50)',
+                    75: 'var(--gray-75)',
+                    100: 'var(--gray-100)',
+                    200: 'var(--gray-200)',
+                    300: 'var(--gray-300)',
+                    400: 'var(--gray-400)',
+                    500: 'var(--gray-500)',
+                    600: 'var(--gray-600)',
+                    700: 'var(--gray-700)',
+                    800: 'var(--gray-800)',
+                    900: 'var(--gray-900)',
+                },
+                selection: 'var(--selection)',
+                border: 'var(--border)',
+            },
+            textColor: {
+                heading: 'var(--heading)',
+                body: 'var(--text-body)',
+                second: 'var(--text-second)',
+                disabled: 'var(--text-disabled)',
+            },
+            backgroundColor: {
+                page: 'var(--page-background)',
+            },
             fontFamily: {
                 maruburi: ['var(--font-maruburi)'],
                 nanumHuman: ['var(--font-nanumhuman)'],
