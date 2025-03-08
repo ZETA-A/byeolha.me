1e1a0b8274ba3dd00b43cb750bdb5e2a128cb72d
ZETA-A <56914556+ZETA-A@users.noreply.github.com>
2025-03-08 14:37:37 +0000

    🛠️ 컴포넌트: PostHeader 개발 완료(CSS 수정 필요... 해보임)

--- components/post/PostHeader.tsx
+++ components/post/PostHeader.tsx
@@ -1,3 +1,14 @@
-export default function PostHeader() {
-    return <div></div>
+import { Post } from '@/config/types';
+import PageHeader from '../layout/PageHeader';
+
+export default function PostHeader({ post }: { post: Post }) {
+    return (
+        <div className="mb-8">
+            <h1>{post.title}</h1>
+            <p className="mb-0.5">
+                {post.dateString} · {post.readingMinutes}min
+            </p>
+            <p>{post.seriesPublicName}</p>
+        </div>
+    );
 }
