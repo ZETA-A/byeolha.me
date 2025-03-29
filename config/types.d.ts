51c2cd17339f129763190d534f3ca2b718110914
ZETA-A <56914556+ZETA-A@users.noreply.github.com>
2025-03-29 05:45:32 +0000

    ⚙️ ENV: 네이버 사이트 소유권 인증 추가

--- config/types.d.ts
+++ config/types.d.ts
@@ -1,3 +1,11 @@
+declare global {
+    namespace NodeJS {
+        export interface ProcessEnv {
+            GA_ID: string;
+            NAVER_SITE_VERIFICATION: string;
+        }
+    }
+}
 export interface PostMatter {
     title: string;
     date: Date;
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     /* config options here */
     transpilePackages: ['next-mdx-remote'],
+    env: {
+        GA_ID: process.env.GA_ID,
+        NAVER_SITE_VERIFICATION: process.env.NAVER_SITE_VERIFICATION,
+    },
 };
 
 export default nextConfig;
