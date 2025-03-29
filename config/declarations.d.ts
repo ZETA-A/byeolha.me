31c35674f9f195954a080f8811e2e9ea6b627479
ZETA-A <56914556+ZETA-A@users.noreply.github.com>
2025-03-29 07:12:04 +0000

    ⚙️ TS: type 롤백

---
+++ config/declarations.d.ts
@@ -0,0 +1,4 @@
+declare module '@microflash/rehype-figure' {
+    const value: any; // TODO: 이 라이브러리에 기여해서 TypeScript 추가하기
+    export default value;
+}
--- config/types.d.ts
+++ config/types.d.ts
@@ -7,12 +7,6 @@ declare global {
     }
 }
 
-declare module '@microflash/rehype-figure' {
-    const value: any; // TODO: 이 라이브러리에 기여해서 TypeScript 추가하기
-    export default value;
-}
-
-
 export interface PostMatter {
     title: string;
     date: Date;
